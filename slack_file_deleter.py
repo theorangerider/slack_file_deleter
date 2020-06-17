cebd72e80d71d09e77a7404c7e95694abc6b623e
TheOrangeRider <theorangerider@users.noreply.github.com>
2020-06-17 00:04:06 -0700

    added show_files_hidden_by_limit to account for tombstoned files

--- slack_file_deleter.py
+++ slack_file_deleter.py
@@ -12,7 +12,13 @@ def main(args):
     now=time.time()
     limit = 60*60*24*180
     timestamp = now - limit
-    result = slack.files.list(ts_to=timestamp)
+    # Slack addded show_files_hidden_by_limit as a flag that will include
+    # files that are now hidden by the 5 GB limit. See 
+    # https://api.slack.com/changelog/2019-03-wild-west-for-files-no-more
+    # This isn't handled by Slacker unless I use the 'get' method.
+    #result = slack.files.list(ts_to=timestamp)
+    result = slack.files.get('files.list',params={'ts_to':timestamp,
+                             'show_files_hidden_by_limit':True})
     for my_file in result.body['files']:
         slack.files.delete(my_file['id'])
 
